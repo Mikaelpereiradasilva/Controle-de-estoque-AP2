8b79db6a1526b3f0c9f8ba7ba62c1618bc9180e7
Joao Ítalo <cabral.italo08@aluno.ifce.edu.br>
2026-09-23 11:32:15 -0300

    Adicionei ao fiscalizar estoque uma busca por CNPJ da escola
    Ainda preciso do menu cadastro finalizada para que a busca seja funcional.

--- crede.py
+++ crede.py
@@ -25,5 +25,19 @@ def autorizacaoCompras():
     util.timer();
 
 def fiscalizarEstoques():
-    print("em breve");
+    Buscaescola = input("Por favor, digite o CNPJ da escola a qual deseja conferir o estoque")
+    #código para buscar o cnpj(só pode ser feito após ter sido feito o código de cadastro)
+    for i in CNPJ:
+        if Buscaescola == CNPJ[i]:
+            util.limpa_tela();
+            print(estoque[i])
+            util.timer();
+            break
+    while True:
+        opcao = input("\nQuando quiser retornar ao Menu Crede, pressione 1.\n")
+        if opcao == 1:
+            menu_principal()
+        else:
+            print("\nOpção inválida! Por favor, digite 1 para voltar ao Menu Crede.\n")
+
     util.timer();

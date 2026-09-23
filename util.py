5537aa000043fb7edd38d4cce36ed3216bf6b3b8
Emanoel <emanoelthy79@gmail.com>
2026-09-23 10:33:23 -0300

    Fiz o menu principal da Crede e criei o módulo "util"

---
+++ util.py
@@ -0,0 +1,12 @@
+import os, time
+
+def cabecalho():
+    print("====================================")
+    print("= Estoque Escolar unificado - v0.1 =")
+    print("====================================")
+
+def limpa_tela():
+    os.system("cls")
+
+def timer():
+    time.sleep(3)
